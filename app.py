6de1ab934f0cc3a5122ba43356065ad42cc79eaa
Ludovico Russo <ludus.russo@gmail.com>
2016-10-17 16:33:13 +0200

    first versione of the app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+ROBOT_NAME = "macbook"
+ROS_VERSION = "jade"
+
+class Robot(Resource):
+    def get(self):
+        return {'name': ROBOT_NAME, 'ros': ROS_VERSION}
+
+api.add_resource(Robot, '/discovery')
+
+@app.route('/')
+def index():
+    return "Hello"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
